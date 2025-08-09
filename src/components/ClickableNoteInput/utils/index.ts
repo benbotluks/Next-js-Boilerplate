d550d5421e3389549a52ea3a6041d1096ce51cce
Ben Luks <benjamin.luks@botpress.com>
2025-08-09 16:00:20 -0400

    refactor

--- src/components/ClickableNoteInput/utils/index.ts
+++ src/components/ClickableNoteInput/utils/index.ts
@@ -1,5 +1,5 @@
-// Utility exports
-export * from './staffCoordinates';
 export * from './notePositioning';
 export * from './noteRendering';
+// Utility exports
+export * from './staffCoordinates';
 export * from './touchHandling';
